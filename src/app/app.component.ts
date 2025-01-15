3833358f9b7d03a00c1dc1be34bb0757f169c296
kqmirasol <kqmirasol@icloud.com>
2025-01-15 10:28:50 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { interval } from 'rxjs';
+import { map } from 'rxjs';
 
 @Component({
+  
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule,],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'pipes-demo.';
+  presentDate = new Date();
+  time$ = interval(1000).pipe(
+    map(() => new Date));
+  testObject = {
+  name: 'Avenir, Christian',
+  age: 21,
+  food: 'Burger'};
+  price: number = 20000;
+  Fruits = ["Apple", "Orange", "Grapes" , "Mango", "Kiwi" , "Pomegranate" ];
+  decimalNum1: number = 8.7589623;
+  decimalNum2: number = 5.43;
+  items: string[] = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry', 'Fig', 'Grape'];
+
 }
